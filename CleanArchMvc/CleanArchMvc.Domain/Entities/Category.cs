5363f59346be34b32bd9cc1a50ac434d6e59fdba
Fernando Ferreira do Carmo Neto <fneto@4bio.com.br>
2023-08-12 19:53:13 -0300

    [feature] Início do Projeto

---
+++ CleanArchMvc/CleanArchMvc.Domain/Entities/Category.cs
@@ -0,0 +1,47 @@
+﻿using CleanArchMvc.Domain.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Domain.Entities;
+
+public sealed class Category
+{
+    public int Id { get; private set; }
+
+    public string Name { get; private set; } = string.Empty;
+
+    public ICollection<Product> Products { get; set; }  
+
+
+    public Category(string name)
+    {
+        ValidateDomain(name);
+    }
+
+    public Category(int id, string name)
+    {
+        DomainExceptionValidation.When(id < 0, "Invalid Id value");
+        Id = id;
+        ValidateDomain(name);
+    }
+
+    public void Update(string name)
+    {
+        ValidateDomain(name);
+    }
+
+    private void ValidateDomain(string name)
+    {
+        DomainExceptionValidation.When(string.IsNullOrEmpty(name), 
+            "Invalid, name.Name is required");
+
+        DomainExceptionValidation.When(name.Length < 3,
+            "Invalid name, to short, minimum 3 characters");
+
+        Name = name;
+
+    }
+}
